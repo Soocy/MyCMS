8262d919603c124138ede3b84dafc0bff5213698
Soocy <schoell93@gmail.com>
2018-01-15 18:53:47 +0100

    sidebar category works

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -33,24 +33,13 @@
                     <?php
                     while ($row = mysqli_fetch_assoc($select_categories_sidebar_query)){
                     $cat_title = $row['cat_title'];
-                    echo "<li><a href=''>{$cat_title}</a></li>";
+                    $cat_id = $row['cat_id'];
+                    echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
                     }
                     ?>
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-           <!-- <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>-->
+
             <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
